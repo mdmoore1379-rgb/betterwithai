020a886d377cbe385ad4474c4e21605a886003c3
mdmoore1379-rgb <mdmoore1379@gmail.com>
2026-06-30 16:19:05 -0500

    Properly import and register EmployeeClarity, Compensation, Benefits and PersonalGoals agents in the main agents dict.

--- agents/master_super_agent.py
+++ agents/master_super_agent.py
@@ -1,3 +1,24 @@
+try:
+    from agents.specialists.employee_clarity_agent import EmployeeClarityAgent
+except:
+    EmployeeClarityAgent = None
+
+try:
+    from agents.specialists.compensation_clarity_agent import CompensationClarityAgent
+except:
+    CompensationClarityAgent = None
+
+try:
+    from agents.specialists.benefits_guide_agent import BenefitsGuideAgent
+except:
+    BenefitsGuideAgent = None
+
+try:
+    from agents.specialists.personal_goals_agent import PersonalGoalsAgent
+except:
+    PersonalGoalsAgent = None
+
+# ... later in the agents: dict ...
             "EmployeeClarityAgent": EmployeeClarityAgent() if EmployeeClarityAgent else None,
             "CompensationClarityAgent": CompensationClarityAgent() if CompensationClarityAgent else None,
             "BenefitsGuideAgent": BenefitsGuideAgent() if BenefitsGuideAgent else None,
